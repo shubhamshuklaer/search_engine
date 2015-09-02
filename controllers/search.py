f628b0b9855b0b9267aac871e0250c03270de798
shubham shukla <shubhamshuklaerssss@gmail.com>
2015-09-02 21:00:27 +0530

    Now handling form in controller

--- controllers/search.py
+++ controllers/search.py
@@ -1,9 +1,12 @@
 # coding: utf8
 # try something like
 def index():
-    form=SQLFORM.factory(Field('search_query', label='', requires=IS_NOT_EMPTY()),buttons=[])
-    if form.process().accepted:
-        search_query=form.vars.search_query
+    search_form=FORM(INPUT(_type="text",_name="search_query",_placeholder="Search term", \
+            _id="search_bar",_class="form-control",requires=IS_NOT_EMPTY()),_class="navbar-form \
+        navbar-left")
+
+    if search_form.process().accepted:
+        search_query=search_form.vars.search_query
         response.flash=search_query
-    return dict(form=form)
+    return dict(search_form=search_form)
 
