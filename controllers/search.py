2de2ba27cd5193eca1fb98b51361aae34fbb3184
shubham shukla <shubhamshuklaerssss@gmail.com>
2015-09-03 20:07:17 +0530

    search bar, paginator and display glued together

--- controllers/search.py
+++ controllers/search.py
@@ -1,5 +1,6 @@
 # coding: utf8
 # try something like
+import math
 def index():
     search_form=FORM(INPUT(_type="text",_name="search_query",_placeholder="Search term", \
             _id="search_bar",_class="form-control",requires=IS_NOT_EMPTY()),LABEL("NLP Mode",_id="nlp_mode_label"),INPUT(_type="checkbox", \
@@ -18,5 +19,17 @@ def index():
     return dict(search_form=search_form, tabs=tabs,num_results=11)
 
 def get_results():
-    return request.vars["type"]+" "+request.vars["page_no"]
+    return request.vars["type"]+" "+request.vars["page_no"]+" "+request.vars["search_bar"]+" "+request.vars["nlp_switch"]
+
+def build_paginator():
+    paginator=""
+    num_results=11
+    num_pages=int(math.ceil(num_results/10.0))
+    for i in range(num_pages):
+        if(i==0):
+            class_name="active";
+        else:
+            class_name="";
+        paginator+=str(LI(A(i+1,_href="#"),_class=class_name))
+    return paginator
 
